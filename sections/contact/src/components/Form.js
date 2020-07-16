3772ba557c7d12c7c3286522b24ca65bc69693b9
Thomas Salah <thomas.salah@gmail.com>
2020-07-16 17:11:30 +0100

    feat: adding status to the form

--- sections/contact/src/components/Form.js
+++ sections/contact/src/components/Form.js
@@ -6,6 +6,7 @@ const INITIAL_STATE = {
   email: '',
   subject: '',
   body: '',
+  status: 'IDLE',
 }
 
 const reducer = (state, action) => {
@@ -15,6 +16,12 @@ const reducer = (state, action) => {
         ...state,
         [action.field]: action.value,
       }
+    case 'updateStatus':
+      return {
+        ...state,
+        status: action.status,
+      }
+    case 'reset':
     default:
       return INITIAL_STATE
   }
@@ -31,54 +38,91 @@ const Form = () => {
     })
   }
 
+  const updateStatus = (status) => {
+    dispatch({
+      type: 'updateStatus',
+      status,
+    })
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
+    updateStatus('PENDING')
     console.log(state)
+
+    setTimeout(() => updateStatus('SUCCESS'), 100)
+  }
+
+  if (state.status === 'SUCCESS') {
+    return (
+      <p className={styles.success}>
+        Message sent!
+        <button
+          className={`${styles.button} ${styles.centered}`}
+          type="reset"
+          onClick={() => dispatch({type: 'reset'})}>
+          Reset
+        </button>
+      </p>
+    )
   }
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <label htmlFor="name" className={styles.label}>
-        Name
-        <input
-          type="text"
-          name="name"
-          className={styles.input}
-          value={state.name}
-          onChange={updateFieldValue('name')}
-        />
-      </label>
-      <label htmlFor="email" className={styles.label}>
-        Email
-        <input
-          type="email"
-          name="email"
-          className={styles.input}
-          value={state.email}
-          onChange={updateFieldValue('email')}
-        />
-      </label>
-      <label htmlFor="subject" className={styles.label}>
-        Subject
-        <input
-          type="text"
-          name="subject"
-          className={styles.input}
-          value={state.subject}
-          onChange={updateFieldValue('subject')}
-        />
-      </label>
-      <label htmlFor="body" className={styles.label}>
-        Body
-        <textarea
-          name="body"
-          className={styles.input}
-          value={state.body}
-          onChange={updateFieldValue('body')}
-        />
-      </label>
-      <button className={styles.button}>Send</button>
-    </form>
+    <>
+      {state.status === 'ERROR' && (
+        <p className={styles.success}>
+          {' '}
+          Something went wrong please, tray again!
+        </p>
+      )}
+
+      <form
+        className={`${styles.form} ${
+          state.status === 'PENDING' && styles.pending
+        }`}
+        onSubmit={handleSubmit}>
+        <label htmlFor="name" className={styles.label}>
+          Name
+          <input
+            type="text"
+            name="name"
+            className={styles.input}
+            value={state.name}
+            onChange={updateFieldValue('name')}
+          />
+        </label>
+        <label htmlFor="email" className={styles.label}>
+          Email
+          <input
+            type="email"
+            name="email"
+            className={styles.input}
+            value={state.email}
+            onChange={updateFieldValue('email')}
+          />
+        </label>
+        <label htmlFor="subject" className={styles.label}>
+          Subject
+          <input
+            type="text"
+            name="subject"
+            className={styles.input}
+            value={state.subject}
+            onChange={updateFieldValue('subject')}
+          />
+        </label>
+        <label htmlFor="body" className={styles.label}>
+          Body
+          <textarea
+            name="body"
+            className={styles.input}
+            value={state.body}
+            onChange={updateFieldValue('body')}
+          />
+        </label>
+        <button className={styles.button}>Send</button>
+      </form>
+    </>
   )
 }
 
